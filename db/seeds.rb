4d88b6086ec30066feb5da18ec4994c7933d431d
taichikoumoto <sbskomoto@gmail.com>
2017-02-21 18:49:29 +0900

    authenticate

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,11 @@ table_names.each do |table_name|
     require(path)
   end
 end
+table_names = %w(administrators)
+table_names.each do |table_name|
+  path = Rails.root.join('db', 'seeds', Rails.env, "#{table_name}.rb")
+  if File.exist?(path)
+    puts "Creating #{table_name}..."
+    require(path)
+  end
+end
