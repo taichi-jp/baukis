39c208b2cba9b7c695966b5db5aed2417eb059d7
taichikoumoto <sbskomoto@gmail.com>
2017-02-22 13:01:43 +0900

    routing

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,5 +1,9 @@
 class Admin::TopController < Admin::Base
   def index
-    render action: 'index'#省略しても同じ
+    if current_administrator
+      render action: 'dashboard'
+    else
+      render action: 'index'#省略しても同じ
+    end
   end
 end
