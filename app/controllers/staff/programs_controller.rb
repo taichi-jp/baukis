716dea47a6b3e74b26ef124aa3ae4f5a130ad39b
taichikoumoto <sbskomoto@gmail.com>
2017-03-17 18:03:58 +0900

    add program view

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -1,5 +1,9 @@
 class Staff::ProgramsController < Staff::Base
   def index
-    @programs = Program.order(application_start_time: :desc).page(params[page])
+    @programs = Program.order(application_start_time: :desc).page(params[:page])
+  end
+
+  def show
+    @program = Program.find(params[:id])
   end
 end
