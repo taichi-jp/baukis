4d1f617ea7fa6a734684265d811a29a3ecc9626d
taichikoumoto <sbskomoto@gmail.com>
2017-04-05 12:19:44 +0900

    add message tree structure

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ActiveRecord::Base
   belongs_to :staff_member
   belongs_to :root, class_name: 'Message', foreign_key: 'root_id'
   belongs_to :parent, class_name: 'Message', foreign_key: 'parent_id'
+  #SimpleTreeで取得するようにする
+  # has_many :children, class_name: 'Message', foreign_key: 'parent_id'
 
   validates :subject, :body, presence: true
   validates :subject, length: { maximum: 80, allow_blank: true }
@@ -16,4 +18,13 @@ class Message < ActiveRecord::Base
   end
 
   default_scope { order(created_at: :desc) }
+
+  attr_accessor :child_nodes
+
+  def tree
+    return @tree if @tree
+    r = root || self
+    messages = Message.where(root_id: r.id).select(:id, :parent_id, :subject)
+    @tree = SimpleTree.new(r, messages)
+  end
 end
